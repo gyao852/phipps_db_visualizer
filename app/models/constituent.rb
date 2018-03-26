21f785176a579aad386a817718a40ed5d74cbcb1
Ashvin Niruttan <anirutta@andrew.cmu.edu>
2018-03-26 18:34:40 -0400

    constituent model skeleton created

---
+++ app/models/constituent.rb
@@ -0,0 +1,3 @@
+class Constituent < ApplicationRecord
+
+end
---
+++ test/system/constituents_test.rb
@@ -0,0 +1,9 @@
+require "application_system_test_case"
+
+class ConstituentsTest < ApplicationSystemTestCase
+  # test "visiting the index" do
+  #   visit constituents_url
+  #
+  #   assert_selector "h1", text: "Constituent"
+  # end
+end

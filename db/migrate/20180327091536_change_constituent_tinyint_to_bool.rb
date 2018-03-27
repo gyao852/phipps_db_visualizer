73779e0013d7693f6378690a253aed0d607e80c5
Ashvin Niruttan <anirutta@andrew.cmu.edu>
2018-03-27 05:27:25 -0400

    corrected model skeleton generated

--- db/migrate/20180327091536_change_constituent_tinyint_to_bool.rb
+++
@@ -1,6 +0,0 @@
-class ChangeConstituentTinyintToBool < ActiveRecord::Migration[5.1]
-  def change
-    change_column :constituent, :do_not_email, :boolean
-    change_column :constituent, :duplicate, :boolean
-  end
-end

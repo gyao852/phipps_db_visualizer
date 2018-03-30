f1bbca83513822b73585a112b40650a911fb12b6
Ashvin Niruttan <anirutta@andrew.cmu.edu>
2018-03-30 15:55:00 -0400

    re commiting validations

--- app/models/address.rb
+++ app/models/address.rb
@@ -9,7 +9,34 @@ class Address < ApplicationRecord
 
  # Validations
  # -----------------------------
+ # validates address_1
+  validates_format_of :address_1, without: /Ave/i, message: "cannot contain abbriviations"
+  validates_format_of :address_1, without: /Dr/i, message: "cannot contain abbriviations"
+  validates_format_of :address_1, without: /St/i, message: "cannot contain abbriviations"
+  validates_format_of :address_1, without: /Apt/i, message: "cannot contain abbriviations"
+  validates_format_of :address_1, without: /Blvd/i, message: "cannot contain abbriviations"
+  validates_format_of :address_1, without: /Rd/i, message: "cannot contain abbriviations"
+  # validates address_2
+  validates_format_of :address_2, without: /Ave/i, message: "cannot contain abbriviations"
+  validates_format_of :address_2, without: /Dr/i, message: "cannot contain abbriviations"
+  validates_format_of :address_2, without: /St/i, message: "cannot contain abbriviations"
+  validates_format_of :address_2, without: /Apt/i, message: "cannot contain abbriviations"
+  validates_format_of :address_2, without: /Blvd/i, message: "cannot contain abbriviations"
+  validates_format_of :address_2, without: /Rd/i, message: "cannot contain abbriviations"
+  # validates city capitalization
+  validates :city, format: { with:/\A[a-zA-Z]+(?:[\s-][a-zA-Z]+)*\z/i , message: "City must be capitalized"}
+   # validates states
+   STATES_LIST = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut',
+   'Delaware', 'District of Columbia', 'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana',
+   'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota',
+   'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico',
+   'New York', 'North Carolina', 'North Dakota', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island',
+   'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington', 'West Virginia',
+   'Wisconsin', 'Wyoming']
+   validates_inclusion_of :state, in: STATES_LIST, message: "is not an option"
 
+   #
+   validates :country, format: { with:/\A[a-zA-Z]+(?:[\s-][a-zA-Z]+)*\z/i , message: "Country must be capitalized"}
 
 
  # Other methods
--- app/models/constituent.rb
+++ app/models/constituent.rb
@@ -11,15 +11,21 @@ class Constituent < ApplicationRecord
   # -----------------------------
 
 
+  #
   # Validations
   # -----------------------------
-  validates :name, presence: true #, format: { with: , message: "Name field cannot contain special characters"}
-  validates :last_group, presence: true #, format: { with: , message: "Last_group field cannot contain special characters"}
-  validates_presence_of :lookup_id
-  # validates :phone, format: { with: /\A\(([0-9]{3})\)[-]([0-9]{3})[-]([0-9]{4})\z/i , message: "format of phone number is incorrect"}
-  # validates :email_id, format: { with:/\A[\w\-\.]+@([\w\-]+\.)+[\w\-]{2,4}[\w\-]{2,4}?\z/i , message: "format of email address is incorrect"}
-  # validates_date :dob
-  # validates :address_or_email
+  validates :lookup_id, presence: true
+  # no validations for suffix
+  # no validations for title
+  # check
+  validates :name, presence: true , format: { with:/\A\w+\-\w+\z/i , message: "Name field cannot contain special characters"}
+  # check
+  validates :last_group, presence: true , format: { with:/\A\w+\-\w+\z/i , message: "Last_group field cannot contain special characters"}
+
+  validates :phone, format: { with: /\A\(([0-9]{3})\)[-]([0-9]{3})[-]([0-9]{4})\z/i , message: "format of phone number is incorrect"}
+  validates :email_id, format: { with:/\A[\w\-\.]+@([\w\-]+\.)+[\w\-]{2,4}[\w\-]{2,4}?\z/i , message: "format of email address is incorrect"}
+  validates_date :dob, before: lambda{Today.date}
+  validates :do_not_email, presence: true
 
   # Other methods
   # -------------

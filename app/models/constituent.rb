702933d39d56fdc3e3d1811f69a8adb064e5216c
Minnie <minniew@andrew.cmu.edu>
2018-03-30 15:15:08 -0400

    started writing factories

--- app/models/constituent.rb
+++ app/models/constituent.rb
@@ -16,9 +16,9 @@ class Constituent < ApplicationRecord
   validates :name, presence: true #, format: { with: , message: "Name field cannot contain special characters"}
   validates :last_group, presence: true #, format: { with: , message: "Last_group field cannot contain special characters"}
   validates_presence_of :lookup_id
-  validates :phone, format: { with: /\A\(([0-9]{3})\)[-]([0-9]{3})[-]([0-9]{4})\z/i , message: "format of phone number is incorrect"}
-  validates :email_id, format: { with:/\A[\w\-\.]+@([\w\-]+\.)+[\w\-]{2,4}[\w\-]{2,4}?\z/i , message: "format of email address is incorrect"}
-  validates_date :dob
+  # validates :phone, format: { with: /\A\(([0-9]{3})\)[-]([0-9]{3})[-]([0-9]{4})\z/i , message: "format of phone number is incorrect"}
+  # validates :email_id, format: { with:/\A[\w\-\.]+@([\w\-]+\.)+[\w\-]{2,4}[\w\-]{2,4}?\z/i , message: "format of email address is incorrect"}
+  # validates_date :dob
   # validates :address_or_email
 
   # Other methods

a6168151c700268fa14884f7563ea94713dba693
Ashvin Niruttan <anirutta@andrew.cmu.edu>
2018-03-30 13:24:33 -0400

    vlidations for constituents done, check on fname and lname format

--- app/models/constituent.rb
+++ app/models/constituent.rb
@@ -10,20 +10,21 @@ class Constituent < ApplicationRecord
   # Scopes
   # -----------------------------
 
-  # 
+  #
   # Validations
   # -----------------------------
   validates :lookup_id, presence: true
   # no validations for suffix
   # no validations for title
-  # we must have a first name ??? check with christine and mike
-  validates :name, presence: true #, format: { with: , message: "Name field cannot contain special characters"}
-  validates :last_group, presence: true #, format: { with: , message: "Last_group field cannot contain special characters"}
-  validates_presence_of :lookup_id
+  # check
+  validates :name, presence: true , format: { with:/\A\w+\-\w+\z/i , message: "Name field cannot contain special characters"}
+  # check
+  validates :last_group, presence: true , format: { with:/\A\w+\-\w+\z/i , message: "Last_group field cannot contain special characters"}
+
   validates :phone, format: { with: /\A\(([0-9]{3})\)[-]([0-9]{3})[-]([0-9]{4})\z/i , message: "format of phone number is incorrect"}
   validates :email_id, format: { with:/\A[\w\-\.]+@([\w\-]+\.)+[\w\-]{2,4}[\w\-]{2,4}?\z/i , message: "format of email address is incorrect"}
-  validates_date :dob
-  # validates :address_or_email
+  validates_date :dob, before: lambda{Today.date}
+  validates :do_not_email, presence: true 
 
   # Other methods
   # -------------
@@ -31,7 +32,5 @@ class Constituent < ApplicationRecord
 
 
   private
-  # address_or_email
-  #   return true
-  # end
+  # callback to check complete record
 end

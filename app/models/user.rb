8891659582797189519553b17a9b6c54cf7514b8
Ashvin Niruttan <anirutta@andrew.cmu.edu>
2018-03-30 16:28:08 -0400

    user validations added

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,12 @@ class User < ApplicationRecord
 
   # Validations
   # -----------------------------
-
+  validates: user_id, presence :true
+  validates: fname, presence :true
+  validates: lname, presence :true
+  validates: email_id, presence :true
+  validates :email_id, format: { with:/\A[\w\-\.]+@([\w\-]+\.)+[\w\-]{2,4}[\w\-]{2,4}?\z/i , message: "format of email address is incorrect"}
+  validates: active, presence :true
 
 
   # Other methods

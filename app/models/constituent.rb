4652f86b100f8c4ffdf384de2018fd7a8c3bc60c
Ashvin Niruttan <anirutta@andrew.cmu.edu>
2018-03-31 09:44:06 -0400

    debuggin models

--- app/models/constituent.rb
+++ app/models/constituent.rb
@@ -18,12 +18,12 @@ class Constituent < ApplicationRecord
   # no validations for suffix
   # no validations for title
   # check
-  validates :name, presence: true , format: { with:/\A\w+\-\w+\z/i , message: "Name field cannot contain special characters"}
+  validates :name, presence: true , format: { with: /\A[A-Z]\w+\-?\w+?\z/ , message: "Name field cannot contain special characters"}
   # check
-  validates :last_group, presence: true , format: { with:/\A\w+\-\w+\z/i , message: "Last_group field cannot contain special characters"}
+  validates :last_group, presence: true , format: { with: /\A[A-Z]\w+\-?\w+?\z/ , message: "Last_group field cannot contain special characters"}
 
-  validates :phone, format: { with: /\A\(([0-9]{3})\)[-]([0-9]{3})[-]([0-9]{4})\z/i , message: "format of phone number is incorrect"}
-  validates :email_id, format: { with:/\A[\w\-\.]+@([\w\-]+\.)+[\w\-]{2,4}[\w\-]{2,4}?\z/i , message: "format of email address is incorrect"}
+  validates :phone, format: { with: /\A\(([0-9]{3})\)[-]([0-9]{3})[-]([0-9]{4})\z/ , message: "format of phone number is incorrect"}
+  validates :email_id, format: { with:/\A[\w\-\.]+@([\w\-]+\.)+[\w\-]{2,4}\z/, message: "format of email address is incorrect"}
   validates_date :dob, before: lambda{Today.date}
   validates :do_not_email, presence: true
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180330200540) do
+ActiveRecord::Schema.define(version: 20180331133035) do
 
   create_table "addresses", force: :cascade do |t|
     t.text "address_id"
@@ -20,7 +20,7 @@ ActiveRecord::Schema.define(version: 20180330200540) do
     t.text "state"
     t.text "zip"
     t.text "country"
-    t.text "type"
+    t.text "address_type"
     t.date "date_added"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false

a762abb50c274d63ea2ed31607b08b8e759c0dda
Ashvin Niruttan <anirutta@andrew.cmu.edu>
2018-04-01 13:26:52 -0400

    address_2 removed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180401165116) do
+ActiveRecord::Schema.define(version: 20180401171739) do
 
   create_table "addresses", force: :cascade do |t|
     t.text "address_id"
@@ -66,6 +66,8 @@ ActiveRecord::Schema.define(version: 20180401165116) do
     t.boolean "duplicate"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "constituent_type"
+    t.text "phone_notes"
   end
 
   create_table "contact_histories", force: :cascade do |t|

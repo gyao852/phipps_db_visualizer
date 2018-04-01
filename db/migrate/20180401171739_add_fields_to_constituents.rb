fdeddb4ea41305008dd0f9e72a13c80da836a330
Ashvin Niruttan <anirutta@andrew.cmu.edu>
2018-04-01 13:23:40 -0400

    added new columns to constituents

--- db/migrate/20180401171739_add_fields_to_constituents.rb
+++ db/migrate/20180401171739_add_fields_to_constituents.rb
@@ -1,4 +1,6 @@
 class AddFieldsToConstituents < ActiveRecord::Migration[5.1]
   def change
+    add_column :constituents, :constituent_type, :text
+    add_column :constituents, :phone_notes, :text
   end
 end

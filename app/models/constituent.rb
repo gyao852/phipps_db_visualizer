3dd2431478ee83b470075754db68c87065322ea7
Minnie <minniew@andrew.cmu.edu>
2018-04-01 20:35:12 -0400

    index users and styling'

--- app/models/constituent.rb
+++ app/models/constituent.rb
@@ -31,10 +31,7 @@ class Constituent < ApplicationRecord
   validates :last_group, presence: true , format: { with: /\A[A-Z]\w+\-?\w+?\z/ , message: "Last_group field cannot contain special characters"}
   validates :phone, format: { with: /\A\(([0-9]{3})\)[-]([0-9]{3})[-]([0-9]{4})\z/ , message: "format of phone number is incorrect"}
   validates :email_id, format: { with:/\A[\w\-\.]+@([\w\-]+\.)+[\w\-]{2,4}\z/, message: "format of email address is incorrect"}
-<<<<<<< HEAD
-=======
 
->>>>>>> de57426c8d7a8384f8794769b472ae8439492adb
   # validates_date :dob, before: lambda{Today.date}
   validates :do_not_email, presence: true
   validates_date :dob, before: Date.today

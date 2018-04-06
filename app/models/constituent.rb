629d90ae336386faa9be8c7e8c7bf0f29de11bdd
Minnie <minniew@andrew.cmu.edu>
2018-04-06 00:31:34 -0400

    uncommented constituent class

--- app/models/constituent.rb
+++ app/models/constituent.rb
@@ -1,4 +1,4 @@
-# class Constituent < ApplicationRecord
+class Constituent < ApplicationRecord
   # Relationships
   # -----------------------------
   # self.primary_key = 'lookup_id'
@@ -40,13 +40,13 @@
    all_addresses = self.addresses
 
    curr = all_addresses.order(date_added: :desc).first
-   if curr.nil?
-     return nil
-     #return "No address available"
-   else
-    return curr.address_1
-   end
- end
+    if curr.nil?
+      return nil
+      #return "No address available"
+    else
+      return curr.address_1
+    end
+  end
 
  def current_membership_level
    #self.lookup_id
@@ -76,6 +76,7 @@
       row[7] = Date.strptime(date_string, '%m/%d/%Y')
       Constituent.create! row.to_hash
     end
+  end
   
 
 

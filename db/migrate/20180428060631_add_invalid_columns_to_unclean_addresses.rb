9b54b1a4f1f8d097c7b754b571fcd926616dd44b
Minnie Wu <minniewu@Minnies-MBP.wv.cc.cmu.edu>
2018-04-28 13:22:50 -0400

    sample files

--- db/migrate/20180428060631_add_invalid_columns_to_unclean_addresses.rb
+++
@@ -1,9 +0,0 @@
-class AddInvalidColumnsToUncleanAddresses < ActiveRecord::Migration[5.1]
-  def change
-    add_column :unclean_addresses, :invalid_addresses_1, :boolean
-    add_column :unclean_addresses, :invalid_cities, :boolean
-    add_column :unclean_addresses, :invalid_states, :boolean
-    add_column :unclean_addresses, :invalid_countries, :boolean
-    add_column :unclean_addresses, :invalid_zips, :boolean
-  end
-end

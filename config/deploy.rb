4cd29506ff3131f141b90094ed30ed30ed8c5b2a
George Yao <gyao@andrew.cmu.edu>
2018-05-01 18:04:55 -0400

    changed deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,6 @@
 require 'bundler/capistrano'
 
-server '139.59.187.53', :web, :app, :db, primary: true
+server 139.59.187.53, :web, :app, :db, primary: true
 set :application, "phipps_db_visualizer"
 set :user, 'phipps'
 set :group, 'admin' # Should this be admin?

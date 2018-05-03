005d921a101564d149744027317b6e3eba09ecd9
Ashvin Niruttan <anirutta@andrew.cmu.edu>
2018-05-03 09:19:05 -0400

    functional reports speed up

--- app/models/constituent.rb
+++ app/models/constituent.rb
@@ -29,9 +29,6 @@ class Constituent < ApplicationRecord
   validates :name, presence: true
   validates :phone, format: {with: /\A\(?\d{3}\)[\s]\d{3}[-]\d{4}\z/i,
     message: "phone number is not valid"}, :allow_blank => true
-  # validates :do_not_email, :inclusion => {:in => [true, false]}
-  # validates_inclusion_of :do_not_email, :in => [true,false]
-  # validates_date :dob, before: Date.today, :allow_blank => true
   validates :email_id, format: { with:/.+@.+\..+/i,
   message: "format of email address is incorrect"}, :allow_blank => true
 
@@ -64,24 +61,27 @@ class Constituent < ApplicationRecord
   # generates report of constituents who have not donated after a certain date and most recent doantions they made
   def self.generate_donations_report(date)
     d = Date.parse(date)
-    sql = "Select constituents.lookup_id, constituents.name, donation_histories.date, donation_histories.amount
+    sql = "Select constituents.lookup_id, constituents.name, max(donation_histories.date) over (partition by constituents.lookup_id) as max_date
            From constituents
            Join donation_histories
            On constituents.lookup_id = donation_histories.lookup_id
-           where  constituents.lookup_id NOT IN (Select constituents.lookup_id
+           where  constituents.lookup_id NOT IN (Select    constituents.lookup_id
                                                            From constituents
                                                            Join donation_histories
                                                            On constituents.lookup_id = donation_histories.lookup_id
                                                            where donation_histories.date IS NOT NULL and donation_histories.date>'#{d}'
                                                            Group by constituents.lookup_id, donation_histories.amount)
-            Order by constituents.lookup_id;"
+            Order by max_date;"
     donations  = ActiveRecord::Base.connection.execute(sql)
-    
+    # donations = Constituent.last_donation_before(d)
+    printed=[]
     result = CSV.generate do |csv|
       csv << ["Constituent", "Email", "Last Donation Date", "Last Donation Amount"]
       donations.each_row do |row|
-        puts row
-        csv << row
+        unless printed.include?row[0]
+          printed<<row[0]
+          csv << row
+        end
       end
     end
     return result
@@ -91,23 +91,23 @@ class Constituent < ApplicationRecord
     return self.contact_histories.chronological.first.date
   end
 
-  # def self.last_contacted_before(date)
-  #   to_contact = []
-  #   Constituent.all.each do |c|
-  #     unless c.contact_histories.nil? || c.contact_histories.empty?
-  #       append = []
-  #       if c.most_recently_contacted_date < date
-  #         append = [c.name, c.email_id, c.do_not_email]
-  #         to_contact.append(append)
-  #       end
-  #     end    
-  #   end
-  #   return to_contact
-  # end
+  def self.last_contacted_before(date)
+    to_contact = []
+    Constituent.joins(:donation_histories).select("DISTINCT constituents.*").each do |c|
+      unless c.contact_histories.nil? || c.contact_histories.empty?
+        append = []
+        if c.most_recently_contacted_date < date
+          append = [c.name, c.email_id, c.do_not_email]
+          to_contact.append(append)
+        end
+      end    
+    end
+    return to_contact
+  end
 
   def self.generate_contact_history_report(date)
     d = Date.parse(date)
-    sql = "Select constituents.lookup_id, constituents.email_id, constituents.do_not_email, contact_histories.date
+    sql = "Select constituents.lookup_id, constituents.email_id, constituents.do_not_email, max(contact_histories.date) over (partition by constituents.lookup_id) as max_date
            From constituents
            Join contact_histories
            On constituents.lookup_id = contact_histories.lookup_id
@@ -117,12 +117,17 @@ class Constituent < ApplicationRecord
                                                On constituents.lookup_id = contact_histories.lookup_id
                                                where contact_histories.date IS NOT NULL and contact_histories.date>'#{d}'
                                                Group by constituents.lookup_id) and constituents.email_id IS NOT NULL and contact_histories.date IS NOT NULL
-            Order by constituents.lookup_id;"
+            Order by max_date;"
     to_contact  = ActiveRecord::Base.connection.execute(sql)
+    # to_contact = Constituent.last_contacted_before(d)
+    printed=[]
     result = CSV.generate do |csv|
       csv << ["Constituent", "Email", "Do Not Email", "date"]
       to_contact.each_row do |row|
-        csv << row
+        unless printed.include?row[0]
+          printed<<row[0]
+          csv << row
+        end
       end
     end
     return result

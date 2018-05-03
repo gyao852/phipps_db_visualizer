811076cd1762e7cfaf4557b93f3e1f02d1a8bdda
Ashvin Niruttan <anirutta@andrew.cmu.edu>
2018-05-02 20:41:02 -0400

    reports speed up

--- app/models/constituent.rb
+++ app/models/constituent.rb
@@ -64,13 +64,17 @@ class Constituent < ApplicationRecord
   # generates report of constituents who have not donated after a certain date and most recent doantions they made
   def self.generate_donations_report(date)
     d = Date.parse(date)
-    sql = "Select constituents.lookup_id, constituents.name, max(donation_histories.date), donation_histories.amount
+    sql = "Select constituents.lookup_id, constituents.name, donation_histories.date, donation_histories.amount
            From constituents
            Join donation_histories
            On constituents.lookup_id = donation_histories.lookup_id
-           where donation_histories.date IS NOT NULL and donation_histories.date < '#{d}'
-           Group by constituents.lookup_id, donation_histories.amount, constituents.name,donation_histories.date
-           Order by date;"
+           where  constituents.lookup_id NOT IN (Select constituents.lookup_id
+                                                           From constituents
+                                                           Join donation_histories
+                                                           On constituents.lookup_id = donation_histories.lookup_id
+                                                           where donation_histories.date IS NOT NULL and donation_histories.date>'#{d}'
+                                                           Group by constituents.lookup_id, donation_histories.amount)
+            Order by donation_histories.date;"
     donations  = ActiveRecord::Base.connection.execute(sql)
     
     result = CSV.generate do |csv|
@@ -87,34 +91,37 @@ class Constituent < ApplicationRecord
     return self.contact_histories.chronological.first.date
   end
 
-  def self.last_contacted_before(date)
-    to_contact = []
-    Constituent.all.each do |c|
-      unless c.contact_histories.nil? || c.contact_histories.empty?
-        append = []
-        if c.most_recently_contacted_date < date
-          append = [c.name, c.email_id, c.do_not_email]
-          to_contact.append(append)
-        end
-      end    
-    end
-    return to_contact
-  end
+  # def self.last_contacted_before(date)
+  #   to_contact = []
+  #   Constituent.all.each do |c|
+  #     unless c.contact_histories.nil? || c.contact_histories.empty?
+  #       append = []
+  #       if c.most_recently_contacted_date < date
+  #         append = [c.name, c.email_id, c.do_not_email]
+  #         to_contact.append(append)
+  #       end
+  #     end    
+  #   end
+  #   return to_contact
+  # end
 
   def self.generate_contact_history_report(date)
     d = Date.parse(date)
-    sql = "Select constituents.lookup_id, constituents.name, max(contact_histories.), contact_histories.
+    sql = "Select constituents.lookup_id, constituents.email_id, constituents.do_not_email, contact_histories.date
            From constituents
            Join contact_histories
            On constituents.lookup_id = contact_histories.lookup_id
-           where donation_histories.date IS NOT NULL and donation_histories.date < '#{d}'
-           Group by constituents.lookup_id, donation_histories.amount, constituents.name,donation_histories.date
-           Order by date;"
-    donations  = ActiveRecord::Base.connection.execute(sql)
-    to_contact = Constituent.last_contacted_before(d)
+           where constituents.lookup_id NOT IN(Select constituents.lookup_id
+                                               From constituents
+                                               Join contact_histories
+                                               On constituents.lookup_id = contact_histories.lookup_id
+                                               where contact_histories.date IS NOT NULL and contact_histories.date>'#{d}'
+                                               Group by constituents.lookup_id) and constituents.email_id IS NOT NULL and contact_histories.date IS NOT NULL
+            Order By contact_histories.date;"
+    to_contact  = ActiveRecord::Base.connection.execute(sql)
     result = CSV.generate do |csv|
-      csv << ["Constituent", "Email", "Do Not Email"]
-      to_contact.each do |row|
+      csv << ["Constituent", "Email", "Do Not Email", "date"]
+      to_contact.each_row do |row|
         csv << row
       end
     end
